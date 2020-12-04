880dc682d2bf7313017a5b6d28c9fcfa19874583
Nv7 <junk4Nv7@gmail.com>
2020-12-04 15:20:30 -0800

    Move into board.go

--- board.go
+++ board.go
@@ -27,3 +27,22 @@ func NewBoard() Board {
 	b.Tris[23] = Triangle{Black: 3}
 	return b
 }
+
+// IsValid checks if a move is valid
+func (b *Board) IsValid(d int, s int, e int) bool { // s = start, e = end
+	if (GetPieces(b.Turn, b.Bar) != 0) && (s >= 0) { // If trying to move from the bar, put a neg num as input
+		return false // Has pieces in the Bar but trying to move pieces not in the bar
+	}
+	absChange := (e - s) * Dir(b.Turn)
+	if absChange != d { // Handles Dice AND going counterclockwise/clockwise
+		return false
+	}
+	if GetPieces(!b.Turn, b.Tris[e]) >= 2 { // Spot is not open
+		return false
+	}
+	if GetPieces(!b.Turn, b.Tris[e]) == 1 { // Blot! - Increase in bar
+		b.Tris[e] = SetPieces(!b.Turn, b.Tris[e], 0)
+		b.Bar = SetPieces(!b.Turn, b.Bar, GetPieces(!b.Turn, b.Bar)+1)
+	}
+	return true
+}
