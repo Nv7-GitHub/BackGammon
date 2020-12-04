53b0c96e6560be1778931cf293609e4178e759ed
Nv7 <junk4Nv7@gmail.com>
2020-12-04 15:18:45 -0800

    moveValid v1.0

--- moveValid.go
+++ moveValid.go
@@ -1,5 +1,19 @@
 package main
 
-func (b *Board) moveValid(d1 int, d2 int, startPos int, endPos int) {
-
+func (b *Board) moveValid(d int, s int, e int) bool { // s = start, e = end
+	if (GetPieces(b.Turn, b.Bar) != 0) && (s >= 0) { // If trying to move from the bar, put a neg num as input
+		return false // Has pieces in the Bar but trying to move pieces not in the bar
+	}
+	absChange := (e - s) * Dir(b.Turn)
+	if absChange != d { // Handles Dice AND going counterclockwise/clockwise
+		return false
+	}
+	if GetPieces(!b.Turn, b.Tris[e]) >= 2 { // Spot is not open
+		return false
+	}
+	if GetPieces(!b.Turn, b.Tris[e]) == 1 { // Blot! - Increase in bar
+		b.Tris[e] = SetPieces(!b.Turn, b.Tris[e], 0)
+		b.Bar = SetPieces(!b.Turn, b.Bar, GetPieces(!b.Turn, b.Bar)+1)
+	}
+	return true
 }
--- util.go
+++ util.go
@@ -6,3 +6,29 @@ import "math/rand"
 func Roll() [2]int {
 	return [2]int{rand.Intn(5) + 1, rand.Intn(5) + 1}
 }
+
+// Dir gets the direction a player should be going based on the turn
+func Dir(turn bool) int {
+	if turn {
+		return 1
+	}
+	return -1
+}
+
+// GetPieces gets the amount of pieces of a certain color given the turn and a triangle
+func GetPieces(turn bool, tri Triangle) int {
+	if turn {
+		return tri.White
+	}
+	return tri.Black
+}
+
+// SetPieces sets the pieces given a turn and a triangle
+func SetPieces(turn bool, tri Triangle, val int) Triangle {
+	if turn {
+		tri.White = val
+	} else {
+		tri.Black = val
+	}
+	return tri
+}
