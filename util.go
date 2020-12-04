b2d05bb128caeddc25bb6c75a6be3a4248675f14
Nv7 <junk4Nv7@gmail.com>
2020-12-04 15:27:04 -0800

    New Roll function

--- util.go
+++ util.go
@@ -3,8 +3,8 @@ package main
 import "math/rand"
 
 // Roll rolls 2 dice
-func Roll() [2]int {
-	return [2]int{rand.Intn(5) + 1, rand.Intn(5) + 1}
+func Roll() (int, int) {
+	return rand.Intn(5) + 1, rand.Intn(5) + 1
 }
 
 // Dir gets the direction a player should be going based on the turn
